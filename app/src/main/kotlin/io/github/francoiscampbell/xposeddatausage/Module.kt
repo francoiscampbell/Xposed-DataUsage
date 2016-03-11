a116b7c449675da6cde712fe6e99fb8d7ea90a9b
Francois Campbell <campbell.francois@gmail.com>
2016-03-11 09:45:10 -0500

    Hook into statusbar layout inflation

--- app/src/main/kotlin/io/github/francoiscampbell/xposeddatausage/Module.kt
+++ app/src/main/kotlin/io/github/francoiscampbell/xposeddatausage/Module.kt
@@ -2,6 +2,7 @@ package io.github.francoiscampbell.xposeddatausage
 
 import de.robv.android.xposed.*
 import de.robv.android.xposed.callbacks.XC_InitPackageResources
+import de.robv.android.xposed.callbacks.XC_LayoutInflated
 import de.robv.android.xposed.callbacks.XC_LoadPackage
 import io.github.francoiscampbell.xposeddatausage.view.DataUsageView
 
@@ -13,7 +14,7 @@ class Module : IXposedHookLoadPackage, IXposedHookInitPackageResources {
         private val PACKAGE_SYSTEM_UI = "com.android.systemui"
     }
 
-    private lateinit var dataUsageView: DataUsageView
+    private var dataUsageView: DataUsageView? = null
 
     override fun handleLoadPackage(lpparam: XC_LoadPackage.LoadPackageParam) {
         if (lpparam.packageName != PACKAGE_SYSTEM_UI) {
@@ -22,13 +23,25 @@ class Module : IXposedHookLoadPackage, IXposedHookInitPackageResources {
 
         XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.policy.Clock", lpparam.classLoader, "updateClock", object : XC_MethodHook() {
             override fun afterHookedMethod(param: MethodHookParam?) {
-                dataUsageView.update()
-                XposedBridge.log("Updating data usage counter to: ${dataUsageView.text}")
+                dataUsageView?.update()
+                XposedBridge.log("Updating data usage counter to: ${dataUsageView?.text}")
             }
         })
     }
 
     override fun handleInitPackageResources(resparam: XC_InitPackageResources.InitPackageResourcesParam) {
-        return
+        if (resparam.packageName != PACKAGE_SYSTEM_UI) {
+            return
+        }
+
+        resparam.res.hookLayout(PACKAGE_SYSTEM_UI, "layout", "status_bar", object : XC_LayoutInflated() {
+            override fun handleLayoutInflated(liparam: LayoutInflatedParam) {
+                if (dataUsageView == null) {
+                    dataUsageView = DataUsageView(liparam.view.context)
+                }
+
+                //TODO attach dataUsageView to statusbar
+            }
+        })
     }
 }
