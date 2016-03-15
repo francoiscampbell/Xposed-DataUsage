519f93fddac4d12fccc261dbcb0e7d24948e18c6
Francois Campbell <campbell.francois@gmail.com>
2016-03-15 16:00:33 -0400

    INetworkStatsService is an interface :/

--- app/src/main/java/android/net/INetworkStatsService.java
+++ app/src/main/java/android/net/INetworkStatsService.java
@@ -3,8 +3,8 @@ package android.net;
 /**
  * Created by francois on 16-03-15.
  */
-public abstract class INetworkStatsService {
-    public abstract void forceUpdate();
+public interface INetworkStatsService {
+    void forceUpdate();
 
-    public abstract long getNetworkTotalBytes(NetworkTemplate template, long lastCycleBoundary, long nextCycleBoundary);
+    long getNetworkTotalBytes(NetworkTemplate template, long lastCycleBoundary, long nextCycleBoundary);
 }
